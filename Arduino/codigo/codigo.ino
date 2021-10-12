574d0dd8dcfffba3a970135d4da3a6fd35c8e60f
AngelPLo <elangelazul360@gmail.com>
2021-10-12 11:53:34 -0500

    Initial commit

---
+++ Arduino/codigo/codigo.ino
@@ -0,0 +1,13 @@
+#include "MPU6050.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  //Busca y conecta con el sensor MPU6050
+
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
