d25cbf3130f7039349bdd045dd1f9bf884b6b290
AngelPLo <elangelazul360@gmail.com>
2021-10-16 10:26:26 -0500

    MPU
    se regresó a MPU y añadió una función de calibración pero no funcionó

--- Arduino/codigo/codigo.ino
+++ Arduino/codigo/codigo.ino
@@ -1,58 +1,45 @@
-//#include "MPU6050.h"
+#include "MPU6050.h"
 #include "Arduino_LSM9DS1.h"
 //#include "LSM9DS1.h"
 #define PI 3.14159
+#define CALIB 6
 
 // genera un objeto de la case MPU6050 
 // con la dirección I2C predeterminada
 //MPU6050 h;
-//MPU6050 inclinometro;
+MPU6050 inclinometro;
 bool conectado = false;
+bool calib_stat = false;
 const uint8_t anguloLimite = 40;
 int factorSensibilidad = 16384; //factor para una sensibilidad +-2g
-float gY;
-float garb;
+float gZ;
 void setup() {
-  //PARA EL LSM9DS1
-  IMU.begin();
-  // // put your setup code here, to run once:
+  pinMode(CALIB, INPUT);
   // // inicializa y conecta con el inclinómetro
-   Serial.begin(115200);
-  // inclinometro.initialize();
-  // conectado = inclinometro.testConnection();
-  // if(conectado){
-  //   inclinometro.setDMPEnabled(false);
-  //   //inclinometro.setMotionDetectionThreshold(50);
-  //   inclinometro.setStandbyXGyroEnabled(true);
-  //   inclinometro.setStandbyYGyroEnabled(true);
-  //   inclinometro.setStandbyZGyroEnabled(true);
-  //   Serial.println(inclinometro.getDeviceID());
-  //   Serial.println(inclinometro.getFullScaleAccelRange());
-  //   Serial.println(inclinometro.getExternalFrameSync());
-  //   inclinometro.CalibrateAccel();
-  //}
+   Serial.begin(38400);
+   inclinometro.initialize();
+   conectado = inclinometro.testConnection();
 }
 
 void loop() {
-  //if (!conectado) return;
-  if(IMU.accelerationAvailable()){
-    IMU.readAcceleration(garb,garb,gY);
-    Serial.println(gY);
-    if (gY<0) gY = -gY;
-    //gY /= float(factorSensibilidad);
+  if (!conectado) return;
 
-    if(gY < sin(float(90-anguloLimite)*2*PI/360)){
-      Serial.println("Se murió :c");
-    }
+  //Debouncing simple para el botón de calibración
+  if (digitalRead(CALIB) && !calib_stat){
+    //Funcion de calibrar
+    calib_stat = true;
   }
-  //gY = inclinometro.getAccelerationY();
- // Serial.println(gY);
- // if (gY<0) gY = -gY;
- // gY /= float(factorSensibilidad);
+  if(!digitalRead(CALIB) && calib_stat){
+    calib_stat = false;
+  }
 
-  //if(gY < sin(float(90-anguloLimite)*2*PI/360)){
-    //Serial.println("Se murió :c");
-  //}
+  gZ = inclinometro.getAccelerationZ();
+  //if (gZ<0) gZ = -gZ;
+  gZ /= float(factorSensibilidad);
+  Serial.println(gZ);
+  if(gZ < sin(float(90-anguloLimite)*2*PI/360)){
+    Serial.println("Se murió :c");
+  }
   //EL VALOR DE Y DEL ACELEROMETRO DEBE VALER 
   
   // put your main code here, to run repeatedly:
