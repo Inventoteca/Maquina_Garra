c820516efc3f0c50c2989aabec594e6439869286
AngelPLo <elangelazul360@gmail.com>
2021-10-21 10:35:59 -0500

    código comentado
    Se comentó el código para que se entienda mejor

--- Arduino/codigo/codigo.ino
+++ Arduino/codigo/codigo.ino
@@ -1,73 +1,58 @@
 #include "MPU6050.h"
 #include <Servo.h>
 #include <math.h>
-#define T_CIERRE 5000
-#define T_DETACH 2500
-#define T_COMP   500
-#define CALIB 5
-#define OUT 8
-#define IN 7
-#define PINS 9
-#define Open true
+
+//DEFINICIONES
+#define T_CIERRE 5000     //Cuando la puerta esté cerrada, recolocará el servo en su posición cada T_CIERRE ms
+#define T_DETACH 2500     //Hace detach del servo para que no ande consumiendo corriente al haber pasado T_DETACH ms de iniciar su movimiento
+#define T_COMP   500      //Cuando se detecte movimiento verifica si dura más de T_COMP ms para decidir si cerrar o no la compuerta
+#define CALIB 5           // Pin para el botón de calibración |activo en bajo|
+#define OUT 8             // Pin de salida para notificar del cierre de la puerta |activo en alto|
+#define IN 7              // Pin de entrada para abrir la puerta |activo en bajo|
+#define PINS 9            // Pin de señal del servomotor
+#define Open true         // Definición de Open como TRUE para simplificar la interpretación del código
 
 // genera un objeto de la case MPU6050 
 // con la dirección I2C predeterminada
 //MPU6050 h;
-Servo compuerta;
-MPU6050 inclinometro;
-bool conectado = false,
-     calib_stat = false,
-     puerta = Open;
-//Angulo permisible respecto a la vertical
-uint8_t anguloLimite = 30;
-uint8_t anguloINF = 0;
-uint8_t anguloSUP = anguloLimite;
-uint8_t bonito = 2;
-const int factorSensibilidad = 2150; //factor para una sensibilidad +-16g
-float gX, 
-      gY, 
-      gZ;
+Servo compuerta;                          // objeto de tipo Servo
+MPU6050 inclinometro;                     // objeto de tipo inclinometro (dirección I2C predeterminada)
+bool conectado = false,                   // representa la conexión del MPU con el arduino (checada al iniciar el Arduino)
+     calib_stat = false,                  // variable que ayuda al debouncing del botón de calibración
+     puerta = Open;                       // estado inicial de la puerta
+uint8_t anguloLimite = 30;                // Angulo permisible respecto a la vertical
+uint8_t anguloINF = 0;                    // Angulo límite mínimo respecto a la vertical
+uint8_t anguloSUP = anguloLimite;         // Angulo límite máximo respecto a la vertical
+uint8_t bonito = 2;                       //Angulo ajustable para que el servo coloque la puerta en una posición exacta
+const int factorSensibilidad = 2150;      //factor de escalamiento para una sensibilidad +-16g del MPU
+float gX,                                 //Variables de aceleración
+      gY,                                 
+      gZ;                                 
       
-unsigned long t_cierre = 0,
+unsigned long t_cierre = 0,               //Variables para el control de tiempos
               t_detachO = 0,
-              t_detachC = 0,
+              t_detachC = 0,              
               t_detachCC = 0,
               t_comparacion = 0,
-              t_act = 0;
+              t_act = 0;                  
       
 void setup() {
   pinMode(CALIB, INPUT_PULLUP);
-  pinMode(OUT, OUTPUT);
   pinMode(IN, INPUT_PULLUP);
+  pinMode(OUT, OUTPUT);
   compuerta.attach(PINS);
   compuerta.write(90+bonito);
-  // inicializa y conecta con el inclinómetro
    Serial.begin(38400);
    conectado = inclinometro.testConnection();
    inclinometro.initialize();
-   inclinometro.setFullScaleAccelRange(MPU6050_ACCEL_FS_16);
+   inclinometro.setFullScaleAccelRange(MPU6050_ACCEL_FS_16);          //Reduce la sensibilidad del acelerómetro
 //   compuerta.detach();
    //leemos los valores de los ángulos límite guardados en la EEPROM
 }
 
 void loop() {
-Serial.print("t: ");
-Serial.print(t_act);
-Serial.print("\t");
-Serial.print("t_dO: ");
-Serial.print(t_detachO);
-Serial.print("\t");
-Serial.print("t_dC: ");
-Serial.print(t_detachC);
-Serial.print("\t");
-Serial.print("t_comp: ");
-Serial.print(t_comparacion);
-Serial.print("\t");
-Serial.print("t_c: ");
-Serial.println(t_cierre);
-Serial.println(" ");
-  
   t_act = millis();
+
   //Manejamos el inevitable overflow de la variable de tiempo
   if(t_act < t_cierre || t_act < t_detachO || t_act < t_detachC ||t_act < t_comparacion){
     t_cierre = t_act;
@@ -83,25 +68,22 @@ Serial.println(" ");
   if(digitalRead(CALIB) && calib_stat){
     calib_stat = false;
   }
+  /////////////////////////////////
 
-  if (puerta == Open){                                //Si la compuerta está abierta
-    if(!enRango(&inclinometro,anguloINF, anguloSUP,500) && digitalRead(IN)){     // y está inclinada
+  if (puerta == Open){                                                            //Si la compuerta está abierta
+    if(!enRango(&inclinometro,anguloINF, anguloSUP,500) && digitalRead(IN)){      // y está inclinada
       compuerta.attach(PINS);                         
       compuerta.write(90+bonito);  
-      puerta = !Open;
-      Serial.println("cerrando");                     //CIERRA LA COMPUERTA
+      puerta = !Open;                                                             //CIERRA LA COMPUERTA
+      Serial.println("cerrando");                     
       if (t_detachC == 0){
         t_detachC = t_act;
       }
-      digitalWrite(OUT, HIGH);  //puerta cerrada
-//      if(t_act - t_detachO >= T_DETACH){
-//        t_detachO = 0;
-//        compuerta.detach();
-//        Serial.println("detach");
-//      }
+      digitalWrite(OUT, HIGH);                                                    // notifica el cierre de la puerta
     }
   }
   
+  // Coloca el servo en la posición de cierre cada 5s si el estado de la puerta indica CERRADO
   if (puerta != Open){
       if (t_cierre == 0){
         t_cierre = t_act;
@@ -117,50 +99,35 @@ Serial.println(" ");
     }
   }
   
-  if(!digitalRead(IN)){                              //Y piden abrirla
+  //Abre la puerta si recibe el comando
+  if(!digitalRead(IN)){                              
     compuerta.attach(PINS); 
     compuerta.write(0 + bonito);
     puerta = Open;
-    Serial.println("abriendo");                     //Abrir la puerta
+    Serial.println("abriendo");                   
     if (t_detachO == 0){
       t_detachO = t_act;
     }
-    digitalWrite(OUT, LOW);     
-//    if(t_act - t_detachC >= T_DETACH){
-//      t_detachC = 0;
-//      Serial.println("detach");
-//      compuerta.detach();
-//    }  
+    digitalWrite(OUT, LOW);                     //Notifica que la puerta está abierta
   }
 
 
       if(t_detachO != 0 && t_act - t_detachO >= T_DETACH ){
         t_detachO = 0;
-//        compuerta.detach();
         Serial.println("detach");
       }
       if(t_detachC != 0 && t_act - t_detachC >= T_DETACH ){
         t_detachC = 0;
-//        compuerta.detach();
         Serial.println("detach");
       }
       if(t_detachCC != 0 && t_act - t_detachCC >= T_DETACH ){
         t_detachCC = 0;
-//        compuerta.detach();
         Serial.println("detach");
       }
-  // if(gZ < sin(float(90-anguloLimite)*2*PI/360)){
-  //   compuerta.attach(PINS);
-  //   compuerta.write(90);
-  //   Serial.println("Se murió :c");
-  //   digitalWrite(OUT, HIGH);  //puerta cerrada
-  // }
-  //EL VALOR DE Y DEL ACELEROMETRO DEBE VALER 
-  
-  // put your main code here, to run repeatedly:
 }
 
 void calibrarReferencia(MPU6050* inclinometro){
+  // Obtiene las componentes del vector actual de la gravedad
   Serial.println("nueva referencia");
   gZ = inclinometro->getAccelerationZ();
   gX = inclinometro->getAccelerationX();
@@ -168,37 +135,16 @@ void calibrarReferencia(MPU6050* inclinometro){
   gZ /= float(factorSensibilidad);
   gX /= float(factorSensibilidad);
   gY /= float(factorSensibilidad);
+
+  //Trigonometría para calcular los valores de los nuevos ángulos permisibles
   if (gZ < 0) gZ = -gZ;
-  // calculamos la componente que nos falta
-   Serial.print("Magnitud coplanar: \t");
-  Serial.println(gX);
-   Serial.print("Magnitud coplanar: \t");
-  Serial.println(gY);
-   Serial.print("Magnitud coplanar: \t");
-  Serial.println(gZ);
   gX = pow(gX,2);
   gY = pow(gY,2);
-  Serial.print("gX^2: ");
-  Serial.println(gX);
-  Serial.print("gY^2: ");
-  Serial.println(gY);
   float coplanar = sqrt(gX + gY);
-  Serial.print("Magnitud coplanar: \t");
-  Serial.println(coplanar);
-  Serial.print("TAN: \t");
-  float tan = atan2(gZ,coplanar);
-  Serial.println(tan);
-  // calculamos el ángulo respecto a Z en el que estamos
-  float angulo = tan > 0? (PI/2) - tan : (PI/2) + tan;
-  Serial.print("Angulo: \t");
-  Serial.println(angulo);
-  //guardamos el offset en memoria EEPROM
+  float _tan = atan2(gZ,coplanar);
+  float angulo = _tan > 0? (PI/2) - _tan : (PI/2) + _tan;
   anguloSUP = (angulo*360/(2*PI) + anguloLimite);
   anguloINF = (angulo*360/(2*PI) - anguloLimite) < 0 ? 0: (angulo*360/(2*PI) - anguloLimite);
-  Serial.print("Angulo INF: \t");
-  Serial.println(anguloINF);
-  Serial.print("Angulo SUP: \t");
-  Serial.println(anguloSUP);
 }
 
 bool enRango(MPU6050 *inclinometro , uint8_t limiteMIN, uint8_t limiteMAX, uint16_t t_tolerancia) {
@@ -207,7 +153,6 @@ bool enRango(MPU6050 *inclinometro , uint8_t limiteMIN, uint8_t limiteMAX, uint1
   if (gZ < 0) gZ = -gZ;
   float anguloMin = sin(float(90-limiteMAX)*2*PI/360);
   float anguloMax = sin(float(90-limiteMIN)*2*PI/360);
-  //Serial.println(gZ);
   if (limiteMIN <= 0)
   {
     if(gZ < anguloMin)
